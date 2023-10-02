d4ea546b84cd90e3554f03433a56993d4f5fce21
MateusCampaner <mateus.campaner@edu.unifil.br>
2023-10-01 21:18:04 -0300

    Revert "Revert "Alteração da tela para verificar analises individuais e criação da tabela de analise-colheita""
    This reverts commit 3d93c9c156dbe615e6a3a25f1883c02ca3a389c4.

--- oraculus/analise/models.py
+++ oraculus/analise/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Analise(models.Model):
     N = models.IntegerField()
@@ -9,5 +10,10 @@ class Analise(models.Model):
     pH = models.FloatField()
     Chuva = models.FloatField()
     Colheita = models.CharField(max_length=50)
+
+class AnaliseSolo(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    analise = models.ForeignKey(Analise, on_delete=models.CASCADE)
+    data_analise = models.DateField() 
 
 
