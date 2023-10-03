c5d9ef971118dc4a4b015531fdf325566313d307
MateusCampaner <mateus.campaner@edu.unifil.br>
2023-10-02 22:15:47 -0300

    Traduçao do csv, remoção de linha no model colheita e refatorações menores

--- oraculus/colheitas/models.py
+++ oraculus/colheitas/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Colheita(models.Model):
     nome_colheita = models.CharField(max_length=50)
-    nome_colheita_dataset = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome_colheita
--- oraculus/recomendar_colheita/views.py
+++ oraculus/recomendar_colheita/views.py
@@ -1,61 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-
 import csv
+import pandas as pd
+
+df=pd.read_csv('crop.csv')
+print(df.describe())
 
 @login_required
 def recomendar_colheita(request):
     return render(request, "recomendar_colheita.html")
 
-def calcular_media_tipo(colheita):
-    colunas = [[] for _ in range(7)] 
+def carregar_dados_csv():
+    df=pd.read_csv('crop.csv')
+    return df
 
-    with open('crop.csv', 'r') as arquivo:
-        leitor_csv = csv.reader(arquivo)
-        next(leitor_csv)  
+    
 
-        for linha in leitor_csv:
-            if len(linha) >= (8) and linha[7] == colheita:
-                for i, valor in enumerate(linha[:7]):
-                    colunas[i].append(float(valor))
-
-    medias = [sum(coluna) / len(coluna) for coluna in colunas]
-
-    return medias
-
-
-def calcular_min_tipo(colheita):
-    colunas = [[] for _ in range(7)] 
-
-    with open('crop.csv', 'r') as arquivo:
-        leitor_csv = csv.reader(arquivo)
-        next(leitor_csv)  
-
-        for linha in leitor_csv:
-            if len(linha) >= (8) and linha[7] == colheita:
-                for i, valor in enumerate(linha[:7]):
-                    colunas[i].append(float(valor))
-
-    minimos = [min(coluna) for coluna in colunas]
-
-    return minimos
-
-def calcular_max_tipo(colheita):
-    colunas = [[] for _ in range(7)] 
-
-    with open('crop.csv', 'r') as arquivo:
-        leitor_csv = csv.reader(arquivo)
-        next(leitor_csv)  
-
-        for linha in leitor_csv:
-            if len(linha) >= (8) and linha[7] == colheita:
-                for i, valor in enumerate(linha[:7]):
-                    colunas[i].append(float(valor))
-
-    minimos = [max(coluna) for coluna in colunas]
-
-    return minimos
 
 
