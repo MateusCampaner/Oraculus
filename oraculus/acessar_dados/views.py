d4ee26b13c67600ae6cc04093bdfc43adf789f95
MateusCampaner <mateus.campaner@edu.unifil.br>
2023-10-31 11:49:21 -0300

    Criação da tela de acessar_dados de analise e modelo

--- oraculus/acessar_dados/views.py
+++ oraculus/acessar_dados/views.py
@@ -5,8 +5,15 @@ from analise.models import Analise, ConfiguracaoAlgoritmo
 
 @login_required
 def acessar_dados(request):
+    return render(request, "acessar_dados.html")
+
+def acessar_dados_analise(request):
     analises = Analise.objects.all()
-    return render(request, "acessar_dados.html", {'analises': analises})
+    return render(request, "acessar_dados_analise.html", {'analises': analises})
+
+def acessar_dados_modelo(request):
+    modelos = ConfiguracaoAlgoritmo.objects.all()
+    return render(request, "acessar_dados_modelo.html", {"modelos": modelos})
 
 def get_analises(request):
     analises = Analise.objects.all()
@@ -15,7 +22,7 @@ def get_analises(request):
 def delete_analises(request, id):
     analises = Analise.objects.get(id=id)
     analises.delete()
-    return redirect(acessar_dados)
+    return redirect(acessar_dados_analise)
 
 def visualizar_analise(request, id):
     analises = Analise.objects.get(id=id)
@@ -28,6 +35,8 @@ def get_modelos(request):
 def delete_modelos(request, id):
     modelos = ConfiguracaoAlgoritmo.objects.get(id=id)
     modelos.delete()
-    return redirect(acessar_dados)
+    return redirect(acessar_dados_modelo)
 
-
+def visualizar_modelo(request, id):
+    modelos = ConfiguracaoAlgoritmo.objects.get(id=id)
+    return render(request, 'visualizar_modelo.html', {'modelos': modelos})
--- oraculus/oraculus/urls.py
+++ oraculus/oraculus/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from home.views import home, ajuda, sobre
 from usuario.views import index
 from django.contrib.auth import views as auth_views
-from inserir_analise.views import inserir_analise, salvar_algoritmo_analise, treinar_algoritmo, configura_algoritmo, salvar_algoritmo #rodar_algoritmo_analise,rodar_analise, roda_analise_solo, detalhes_analise_solo
+from inserir_analise.views import inserir_analise, salvar_algoritmo_analise, treinar_algoritmo, configura_algoritmo, salvar_algoritmo 
 from recomendar_colheita.views import recomendar_colheita, enviar_colheita, calcular_valores, gerar_relatorio_colheita
-from acessar_dados.views import acessar_dados, delete_analises, visualizar_analise, delete_modelos, get_modelos
+from acessar_dados.views import acessar_dados, delete_analises, visualizar_analise, delete_modelos, acessar_dados_analise, acessar_dados_modelo, visualizar_modelo
 from visualizar_analise.views import delete_analise, gerar_relatorio_analise
 from resultado.views import resultado
 
@@ -32,15 +32,14 @@ urlpatterns = [
     path("treinar_algoritmo", treinar_algoritmo, name="treinar_algoritmo"),
     path("configura_algoritmo", configura_algoritmo, name="configura_algoritmo"),
     path("salvar_algoritmo", salvar_algoritmo, name="salvar_algoritmo"),
-    #path("rodar_algoritmo_analise/", rodar_algoritmo_analise, name="rodar_algoritmo_analise"),
-    #path("rodar_analise/", rodar_analise, name="rodar_analise"),
-    #path("roda_analise_solo/", roda_analise_solo, name="roda_analise_solo"),
-    #path("detalhes_analise_solo/", detalhes_analise_solo, name="detalhes_analise_solo"),
-    
+
     #Crud de Acessar Dados
+    path("acessar_dados_analise", acessar_dados_analise, name="acessar_dados_analise"),
+    path("acessar_dados_modelo", acessar_dados_modelo, name="acessar_dados_modelo"),
     path("visualizar_analise/<int:id>/", visualizar_analise, name="visualizar_analise"),
     path("delete_analise/<int:id>/", delete_analise, name="delete_analise"),
     path("gerar_relatorio_analise", gerar_relatorio_analise, name="gerar_relatorio_analise"),
+    path("visualizar_modelo/<int:id>/", visualizar_modelo, name="visualizar_modelo"),
     path("delete_modelos/<int:id>/", delete_modelos, name="delete_modelos"),
 
     #Recomendar Colheita
